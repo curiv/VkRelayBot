8a4bc8add170e5b94de7e81879fd63dd3457ed42
Curiv <>
2020-04-17 22:03:30 +0500

    Added integration with sqlite db and now I can answer to the messages from vk throught telegram

--- telewarden.py
+++ telewarden.py
@@ -15,15 +15,29 @@ def handle_start_help(msg):
 
 @bot.message_handler(commands=['help'])
 def handle_start_help(msg):
-    pass
+    bot.reply_to(msg,"Hi I'm a Vk-relay bot.\n" \
+            "I'm really in the development state now"
+    )
 
-@bot.message_handler(commands=['send'])
-def send_message(msg):
-   session = vk.Session(access_token=VK_TOKEN)
-   api     = vk.API(session, v=VK_API_VERSION, lang='ru', timeout=1)
-   api.messages.send(
-            random_id=random.randrange(1,2**16), peer_id=62136092, message=":)"
+# Handle all text messages
+@bot.message_handler(func=lambda m: True)
+def message_handle(msg):
+
+    # check for reply and then send message
+    if msg.reply_to_message:
+
+        # Prepare some credential to anser in VK
+        peer_id=msg.reply_to_message.text.split()[1]
+        message_id=msg.reply_to_message.text.split()[2]
+
+        session = vk.Session(access_token=VK_TOKEN)
+        api     = vk.API(session, v=VK_API_VERSION, lang='ru', timeout=1)
+        api.messages.send(
+                random_id=random.randrange(1,2**16), peer_id=peer_id, message=msg.text,  reply_to=message_id
         )
+
+        bot.reply_to(msg, "Message sent succesfully!")
+        print("[D] message sent to", peer_id)
 
 if __name__ == '__main__':
     print('Vk relaybot started')
